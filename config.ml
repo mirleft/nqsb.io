cf962bc700a16049fb0ee1933e0a11e9b7c3f3a1
Hannes Mehnert <hannes@mehnert.org>
2016-12-17 15:55:20 +0000

    syslog support, also result everywhere

--- config.ml
+++ config.ml
@@ -11,21 +11,23 @@ let net =
     (socket_stackv4 [Ipaddr.V4.any])
     (static_ipv4_stack ~config:address default_network)
 
+let logger =
+  syslog_udp
+    (syslog_config ~truncate:1484 "nqsb.io" (Ipaddr.V4.of_string_exn "198.167.222.206"))
+    net
+
 let () =
   let packages = [
     package ~sublibs:["mirage"] "tls";
     package "tyxml";
-    package ~sublibs:["mirage"] "logs-syslog";
     package ~sublibs:["lwt"] "logs"
   ] in
   register "nqsb.io" [
     foreign
-      ~deps:[abstract nocrypto]
+      ~deps:[abstract nocrypto; abstract logger]
       ~packages
       "Unikernel.Main"
-      (console @-> pclock @-> stackv4 @-> kv_ro @-> kv_ro @-> job)
-    $ default_console
-    $ default_posix_clock
+      (stackv4 @-> kv_ro @-> kv_ro @-> job)
     $ net
     $ crunch "tls"
     $ crunch "disk"
--- unikernel.ml
+++ unikernel.ml
@@ -1,11 +1,10 @@
 open Lwt.Infix
 open V1_LWT
 
-module Main (C : CONSOLE) (CLOCK: V1.PCLOCK) (S : STACKV4) (KEYS : KV_RO) (KV : KV_RO) =
+module Main (S : STACKV4) (KEYS : KV_RO) (KV : KV_RO) =
 struct
   module TCP   = S.TCPV4
   module TLS   = Tls_mirage.Make (TCP)
-  module UDPLOG = Logs_syslog_mirage.Udp(C)(CLOCK)(S.UDPV4)
 
   let read_key kv name =
     KEYS.size kv name >>= function
@@ -51,13 +50,17 @@ struct
     [ header "application/pdf" ; data ]
 
   let tls_accept ~tag ~f cfg tcp =
-    let peer = TCP.dst tcp in
-    let log  = log tag peer in
+    let ip, port = TCP.dst tcp in
     let with_tls_server k = TLS.server_of_flow cfg tcp >>= k
     in
     with_tls_server @@ function
-    | Error _ -> log "TLS failed" >>= fun () -> TCP.close tcp
+    | Error e ->
+      Logs_lwt.warn (fun m -> m "[%s] %s:%d error %a"
+                        tag (Ipaddr.V4.to_string ip) port
+                        TLS.pp_write_error e) >>= fun () ->
+      TCP.close tcp
     | Ok tls ->
+      let log = log tag (ip, port) in
       f log tls >>= fun data ->
       TLS.writev tls data >>= fun _ ->
       TLS.close tls
@@ -66,17 +69,20 @@ struct
       ~status:"HTTP/1.1 301 Moved permanently"
       [ ("location", "https://nqsb.io") ]
 
-  let h_notice ~tag =
-    fun tcp ->
-      let log = log tag (TCP.dst tcp) in
-      TCP.write tcp moved_permanently >>= function
-      | Error `Closed -> log "write error, closed"
-      | Error (`Msg str) -> log ("write error" ^ str)
-      | Ok () -> log "responded" >>= fun () -> TCP.close tcp
+  let h_notice ~tag tcp =
+    let ip, port = TCP.dst tcp in
+    TCP.write tcp moved_permanently >>= function
+    | Error e ->
+      Logs_lwt.warn (fun m -> m "[%s] %s:%d error %a"
+                        tag (Ipaddr.V4.to_string ip) port
+                        TCP.pp_write_error e) >>= fun () ->
+      TCP.close tcp
+    | Ok () ->
+      log tag (ip, port) "TCP responded" >>= fun () -> TCP.close tcp
 
   let dispatch nqsb usenix tron log tls =
     match TLS.epoch tls with
-    | Error _ -> log "error while getting epoch, serving nqsb.io" >|= fun () -> nqsb
+    | Error () -> log "error while getting epoch, serving nqsb.io" >|= fun () -> nqsb
     | Ok e ->
       match e.Tls.Core.own_name with
       | Some "usenix15.nqsb.io" -> log "serving usenix pdf" >|= fun () -> usenix
@@ -85,7 +91,7 @@ struct
       | Some x -> log ("SNI is " ^ x ^ ", servion nqsb.io")  >|= fun () -> nqsb
       | None -> log "no sni, serving nqsb.io" >|= fun () -> nqsb
 
-  let start con clock stack keys kv _ =
+  let start stack keys kv _ _ =
     let d_nqsb = [ header "text/html;charset=utf-8" ; Page.render ] in
     read_pdf kv "nqsbtls-usenix-security15.pdf" >>= fun d_usenix ->
     read_pdf kv "tron.pdf" >>= fun d_tron ->
@@ -95,12 +101,6 @@ struct
     read_cert keys "usenix15" >>= fun c_usenix ->
     read_cert keys "tron" >>= fun c_tron ->
     let config = Tls.Config.server ~certificates:(`Multiple_default (c_nqsb, [ c_usenix ; c_tron])) () in
-
-    let reporter =
-      let ip = Ipaddr.V4.of_string_exn "198.167.222.206" in
-      UDPLOG.create con clock (S.udpv4 stack) ~hostname:"web.nqsb.io" ip ()
-    in
-    Logs.set_reporter reporter ;
 
     S.listen_tcpv4 stack ~port:80 (h_notice ~tag:"HTTP") ;
     S.listen_tcpv4 stack ~port:443 (tls_accept ~tag:"HTTPS" ~f config) ;
