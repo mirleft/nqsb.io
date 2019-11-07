1e90e3414361bc831bf487172d300133cb0e0b83
Hannes Mehnert <hannes@mehnert.org>
2019-11-07 22:05:31 +0100

    use monitoring and logging on separate stack

--- config.ml
+++ config.ml
@@ -1,9 +1,5 @@
 open Mirage
 
-let net = generic_stackv4 default_network
-
-let logger = syslog_udp ~config:(syslog_config "nqsb.io") net
-
 let dns_key =
   let doc = Key.Arg.info ~doc:"nsupdate key (name:type:value,...)" ["dns-key"] in
   Key.(create "dns-key" Arg.(required string doc))
@@ -20,28 +16,44 @@ let key_seed =
   let doc = Key.Arg.info ~doc:"certificate key seed" ["key-seed"] in
   Key.(create "key-seed" Arg.(required string doc))
 
-let keys = Key.[
-    abstract dns_key ; abstract dns_server ; abstract dns_port ;
-    abstract key_seed
+let monitor =
+  let doc = Key.Arg.info ~doc:"monitor host IP" ["monitor"] in
+  Key.(create "monitor" Arg.(opt ipv4_address Ipaddr.V4.unspecified doc))
+
+let syslog =
+  let doc = Key.Arg.info ~doc:"syslog host IP" ["syslog"] in
+  Key.(create "syslog" Arg.(opt ipv4_address Ipaddr.V4.unspecified doc))
+
+let name =
+  let doc = Key.Arg.info ~doc:"Name of the unikernel" ["name"] in
+  Key.(create "name" Arg.(opt string "nqsb.io" doc))
+
+let keys = [
+  Key.abstract dns_key ; Key.abstract dns_server ; Key.abstract dns_port ; Key.abstract key_seed ;
+  Key.abstract name ; Key.abstract monitor ; Key.abstract syslog
   ]
+
+let net = generic_stackv4 default_network
+
+let management_stack = generic_stackv4 ~group:"management" (netif ~group:"management" "management")
 
 let () =
   let packages = [
     package ~sublibs:["mirage"] "tls";
     package "tyxml";
     package "logs" ;
-    package "dns-mirage-certify" ;
-    package ~min:"0.2.1" "logs-syslog" ;
+    package ~sublibs:["mirage"] "dns-certify" ;
+    package ~sublibs:["mirage"] ~min:"0.2.1" "logs-syslog" ;
     package ~min:"3.7.1" "tcpip" ;
     package ~min:"2.0.0" "mirage-kv" ;
     package "monitoring-experiments" ;
   ] in
   register "nqsb.io" [
     foreign
-      ~deps:[ abstract nocrypto ; abstract logger ; abstract app_info ]
+      ~deps:[ abstract nocrypto ; abstract app_info ]
       ~keys
       ~packages
       "Unikernel.Main"
-      (random @-> pclock @-> mclock @-> time @-> stackv4 @-> kv_ro @-> kv_ro @-> job)
-    $ default_random $ default_posix_clock $ default_monotonic_clock $ default_time $ net $ crunch "tls" $ crunch "disk"
+      (console @-> random @-> time @-> mclock @-> pclock @-> stackv4 @-> kv_ro @-> stackv4 @-> job)
+    $ default_console $ default_random $ default_time $ default_monotonic_clock $ default_posix_clock $ net $ crunch "disk" $ management_stack
   ]
--- unikernel.ml
+++ unikernel.ml
@@ -1,39 +1,34 @@
 open Lwt.Infix
-open Mirage_types_lwt
 
-module Main (R : RANDOM) (P : PCLOCK) (C : MCLOCK) (T : TIME) (S : STACKV4) (KEYS : KV_RO) (KV : KV_RO) = struct
+module Main (C : Mirage_console.S) (R : Mirage_random.S) (T : Mirage_time.S) (M : Mirage_clock.MCLOCK) (P : Mirage_clock.PCLOCK) (S : Mirage_stack.V4) (KV : Mirage_kv.RO) (Management : Mirage_stack.V4) = struct
   module TCP   = S.TCPV4
   module TLS   = Tls_mirage.Make (TCP)
-  module D = Dns_mirage_certify.Make(R)(P)(T)(S)
-  module X = Tls_mirage.X509(KEYS)(P)
-  module M = Monitoring_experiments.M.S(T)(P)(C)(S)
 
-  type s = {
-    mutable http : int ;
-    mutable nqsb : int ;
-    mutable usenix : int ;
-    mutable tron : int ;
-    mutable default : int ;
-    mutable none : int ;
-  }
-  let stat = { http = 0 ; nqsb = 0 ; usenix = 0 ; tron = 0 ; default = 0 ; none = 0 }
+  let create ~f =
+    let data : (string, int) Hashtbl.t = Hashtbl.create 7 in
+    (fun x ->
+       let key = f x in
+       let cur = match Hashtbl.find_opt data key with
+         | None -> 0
+         | Some x -> x
+       in
+       Hashtbl.replace data key (succ cur)),
+    (fun () ->
+       let data, total =
+         Hashtbl.fold (fun key value (acc, total) ->
+             (Metrics.uint key value :: acc), value + total)
+           data ([], 0)
+       in
+       Metrics.uint "total" total :: data)
 
-  let metrics =
+  let counter_metrics ~f name =
     let open Metrics in
-    let doc = "nqsbio statistics" in
-    let data stat =
-      Data.v
-        [ int "http" stat.http
-        ; int "nqsb" stat.nqsb
-        ; int "usenix" stat.usenix
-        ; int "tron" stat.tron
-        ; int "none" stat.none
-        ; int "default" stat.default ]
-    in
-    Src.v ~doc ~tags:Metrics.Tags.[] ~data "nqsbio"
+    let doc = "Counter metrics" in
+    let incr, get = create ~f in
+    let data thing = incr thing; Data.v (get ()) in
+    Src.v ~doc ~tags:Metrics.Tags.[] ~data name
 
-  let m () =
-    Metrics.add metrics (fun x -> x) (fun d -> d stat)
+  let http_status = counter_metrics ~f:(fun x -> x) "nqsbio"
 
   let http_header ~status xs =
     let headers = List.map (fun (k, v) -> k ^ ": " ^ v) xs in
@@ -57,101 +52,71 @@ module Main (R : RANDOM) (P : PCLOCK) (C : MCLOCK) (T : TIME) (S : STACKV4) (KEY
       let cs = Cstruct.of_string data in
       Lwt.return [ header "application/pdf" (Cstruct.len cs) ; cs ]
 
-  let stored_tags = Lwt.new_key ()
-
-  let add_tag ntag nval =
-    let other = match Lwt.get stored_tags with
-      | None -> Logs.Tag.empty
-      | Some x -> x
-    in
-    Logs.Tag.add ntag nval other
-
-  let with_tag ntag nval f =
-    Lwt.with_value stored_tags (Some (add_tag ntag nval)) f
-
-  let peer_tag : (Ipaddr.V4.t * int) Logs.Tag.def =
-    Logs.Tag.def "peer" ~doc:"connection endpoint"
-      Fmt.(pair ~sep:(unit ":") Ipaddr.V4.pp int)
-
-  let tls_tag : Tls.Core.epoch_data Logs.Tag.def =
-    Logs.Tag.def "tls" ~doc:"TLS parameters"
-      (fun ppf epoch ->
-         let open Tls.Core in
-         let open Sexplib.Sexp in
-         Fmt.pf ppf "%a,%a,%a,extended_ms=%a"
-           pp (sexp_of_tls_version epoch.protocol_version)
-           pp (Tls.Ciphersuite.sexp_of_ciphersuite epoch.ciphersuite)
-           Fmt.(option ~none:(unit "no SNI") string) epoch.own_name
-           Fmt.bool epoch.extended_ms)
-
   let tls_accept ~f cfg tcp =
-    with_tag peer_tag (TCP.dst tcp)
-      (fun () ->
-         TLS.server_of_flow cfg tcp >>= function
-         | Error e ->
-           Logs.warn (fun m -> m ?tags:(Lwt.get stored_tags) "TLS error %a"
-                         TLS.pp_write_error e);
-           TCP.close tcp
-         | Ok tls ->
-           let data = f tls in
-           TLS.writev tls data >>= fun _ ->
-           TLS.close tls)
+    TLS.server_of_flow cfg tcp >>= function
+    | Error e ->
+      Logs.warn (fun m -> m "TLS error %a" TLS.pp_write_error e);
+      TCP.close tcp
+    | Ok tls ->
+      let data = f tls in
+      (TLS.writev tls data >|= function
+        | Ok () -> ()
+        | Error we ->
+          Logs.warn (fun m -> m "TLS write error %a" TLS.pp_write_error we)) >>= fun () ->
+      TLS.close tls
 
   let moved_permanently =
     http_header ~status:"HTTP/1.1 301 Moved permanently"
       [ ("location", "https://nqsb.io") ]
 
   let h_notice tcp =
-    let tags = add_tag peer_tag (TCP.dst tcp) in
-    stat.http <- succ stat.http; m ();
+    Metrics.add http_status (fun x -> x) (fun d -> d "http");
     TCP.write tcp moved_permanently >>= fun r ->
     (match r with
-     | Error e -> Logs.warn (fun m -> m ~tags "TCP error %a" TCP.pp_write_error e)
-     | Ok () -> Logs.info (fun m -> m ~tags "HTTP responded")) ;
+     | Error e -> Logs.warn (fun m -> m  "TCP error %a" TCP.pp_write_error e)
+     | Ok () -> ());
     TCP.close tcp
 
   let dispatch nqsb usenix tron tls =
     match TLS.epoch tls with
     | Error () ->
-      Logs.warn (fun m -> m ?tags:(Lwt.get stored_tags)
-                    "error while retrieving epoch, serving nqsb.io") ;
+      Logs.warn (fun m -> m "error while retrieving epoch, serving nqsb.io") ;
       nqsb
     | Ok e ->
-      let tags = add_tag tls_tag e in
-      match e.Tls.Core.own_name with
-      | Some "usenix15.nqsb.io" ->
-        Logs.info (fun m -> m ~tags "serving usenix pdf") ;
-        stat.usenix <- succ stat.usenix; m ();
-        usenix
-      | Some "tron.nqsb.io" ->
-        Logs.info (fun m -> m ~tags "serving tron pdf") ;
-        stat.tron <- succ stat.tron; m ();
-        tron
-      | Some "nqsb.io" ->
-        Logs.info (fun m -> m ~tags "serving nqsb.io") ;
-        stat.nqsb <- succ stat.nqsb; m ();
-        nqsb
-      | Some x ->
-        Logs.info (fun m -> m ~tags "unknown SNI (%s), serving nqsb.io" x) ;
-        stat.default <- succ stat.default; m ();
-        nqsb
-      | None ->
-        Logs.info (fun m -> m ~tags "no sni, serving nqsb.io") ;
-        stat.none <- succ stat.none; m ();
-        nqsb
+      let name, data =
+        match e.Tls.Core.own_name with
+        | Some "usenix15.nqsb.io" -> "usenix", usenix
+        | Some "tron.nqsb.io" -> "tron", tron
+        | Some "nqsb.io" -> "nqsb", nqsb
+        | Some x -> x, nqsb
+        | None -> "none", nqsb
+      in
+      Metrics.add http_status (fun x -> x) (fun d -> d name);
+      data
 
-  let start _ _mclock _pclock _time stack keys kv _ _ info =
-    Logs.info (fun m -> m "used packages: %a"
-                  Fmt.(Dump.list @@ pair ~sep:(unit ".") string string)
-                  info.Mirage_info.packages) ;
-    Logs.info (fun m -> m "used libraries: %a"
-                  Fmt.(Dump.list string) info.Mirage_info.libraries) ;
-    X.certificate keys (`Name "nqsbio") >>= fun (certs, key) ->
-    let c = `Single (certs, key) in
-    M.create stack ~hostname:"nqsb.nqsbio" c;
-    let hostname = Domain_name.of_string_exn "nqsb.io"
+  module D = Dns_certify_mirage.Make(R)(P)(T)(S)
+  module Monitoring = Monitoring_experiments.Make(T)(Management)
+  module Syslog = Logs_syslog_mirage.Udp(C)(P)(Management)
+
+  let start c _random _time _mclock _pclock stack kv management _ info =
+    let hostname = Key_gen.name ()
+    and syslog = Key_gen.syslog ()
+    and monitor = Key_gen.monitor ()
+    in
+    if Ipaddr.V4.compare syslog Ipaddr.V4.unspecified = 0 then
+      Logs.warn (fun m -> m "no syslog specified, dumping on stdout")
+    else
+      Logs.set_reporter (Syslog.create c management syslog ~hostname ());
+    if Ipaddr.V4.compare monitor Ipaddr.V4.unspecified = 0 then
+      Logs.warn (fun m -> m "no monitor specified, not outputting statistics")
+    else
+      Monitoring.create ~hostname monitor management;
+    List.iter (fun (p, v) -> Logs.app (fun m -> m "used package: %s %s" p v))
+      info.Mirage_info.packages;
+    let hostname = Domain_name.(of_string_exn "nqsb.io" |> host_exn)
     and additional_hostnames =
-      List.map Domain_name.of_string_exn[ "tron.nqsb.io" ; "usenix15.nqsb.io" ]
+      List.map (fun s -> Domain_name.(of_string_exn s |> host_exn))
+        [ "tron.nqsb.io" ; "usenix15.nqsb.io" ]
     in
     D.retrieve_certificate ~ca:`Production
       stack ~dns_key:(Key_gen.dns_key ())
@@ -167,7 +132,6 @@ module Main (R : RANDOM) (P : PCLOCK) (C : MCLOCK) (T : TIME) (S : STACKV4) (KEY
       read_pdf kv "nqsbtls-usenix-security15.pdf" >>= fun d_usenix ->
       read_pdf kv "tron.pdf" >>= fun d_tron ->
       let f = dispatch d_nqsb d_usenix d_tron in
-
       S.listen_tcpv4 stack ~port:80 h_notice ;
       S.listen_tcpv4 stack ~port:443 (tls_accept ~f config) ;
       S.listen stack
